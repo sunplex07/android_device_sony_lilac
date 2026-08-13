ba31af3d81e2b780c0495f0dd4002a9bf2aca950
sunplex07 <130389250+sunplex07@users.noreply.github.com>
2026-08-13 15:24:27 -0700

    shorten ota endpoint url

--- device.mk
+++ device.mk
@@ -62,7 +62,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_ADB_KEYS := device/sony/lilac/adb_keys
 
 PRODUCT_PRODUCT_PROPERTIES += \
-    lineage.updater.uri=https://raw.githubusercontent.com/sunplex07/lineageos-lilac-releases/main/api/v2/devices/{device}/{type}.json
+    lineage.updater.uri=https://raw.githubusercontent.com/sunplex07/lineageos-lilac-releases/main/{device}.json
 
 PRODUCT_PACKAGES += \
     DeskClockWidgetColorFix
